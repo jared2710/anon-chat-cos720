0bf62a88d8b1fee6c9a879eb7c5cfbb60368e642
jared2710 <jaredoreilly@gmail.com>
2020-05-16 17:42:58 +0200

    Added get for home page

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+app.get("/", (req, res) =>
+{
+	res.json({"response" : "hello"});
+});
+
 app.post("/", (req, res) =>
 {
 	res.json({"response" : "hello"});
