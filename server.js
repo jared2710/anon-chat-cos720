0ef9db1be919367940c6b890d3512009967d476f
jared2710 <jaredoreilly@gmail.com>
2020-05-16 17:50:21 +0200

    Added more console logs

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) =>
-{
+{	
+	console.log("Got GET request for /");
 	res.json({"response" : "hello"});
+	console.log("Replied to GET request for /");
 });
 
 app.post("/", (req, res) =>
 {
+	console.log("Got POST request for /");
 	res.json({"response" : "hello"});
+	console.log("Replied to POST request for /");
 });
 
 app.listen(4000, () =>
