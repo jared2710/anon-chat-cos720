904cc29018598f61d31d3779af4597966e30cbbb
jared2710 <jaredoreilly@gmail.com>
2020-05-19 12:23:47 +0200

    Better pos

--- chat.js
+++ chat.js
@@ -41,19 +41,13 @@ var authStringToUsername = function (auth, names)
 	for(var i = 0; i < 9; i = i + 3)
 	{
 		//console.log(hash[i]);
-		var char1 = hash[i];
-		var char2 = hash[i+1];
-		var char3 = hash[i+2];
-		console.log(char1 + "|" + char2 + "|" + char3);
+		var hex = hash[i] + hash[i+1] + hash[i+2];
+		console.log(hex);
 
-		char1 = parseInt(Number("0x" + char1), 10);
-		char2 = parseInt(Number("0x" + char2), 10);
-		char3 = parseInt(Number("0x" + char3), 10);
-		console.log(char1 + "|" + char2 + "|" + char3);
-
-		var pos = char1*16*16 + char2*16 + char3;
+		var pos = parseInt(Number("0x" + hex), 10);
 		console.log(pos);
-		pos += i*16*16*16;
+
+		pos += (i/3)*16*16*16;
 		console.log(pos);
 
 
@@ -61,9 +55,9 @@ var authStringToUsername = function (auth, names)
 		console.log(toAdd);
 		
 		username += toAdd + " ";
-		console.log(username.substring(0, -1));
+		console.log(username);
 	}
-	return username.substring(0, -1);
+	return username.substring(0, username.length-1);
 }
 
 var getChatroomMessages = function (chatroom)
