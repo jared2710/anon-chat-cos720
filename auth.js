b5fbb8dfa1812650fbae896a7a789e303afd905c
jared2710 <jaredoreilly@gmail.com>
2020-05-19 12:18:17 +0200

    Moved functions around

--- auth.js
+++ auth.js
@@ -1,4 +1,3 @@
-var crypto = require("crypto");
 
 var CHAT = require('./chat');
 
@@ -26,48 +25,6 @@ var isValidChatroom = function (chatroom)
 	}
 	return false;
 }
-
-var sha256 = function (text)
-{
-	return crypto.createHash('sha256').update(text).digest('hex');
-}
-
-var authStringToUsername = function (auth, names)
-{
-	console.log(auth);
-	var hash = sha256(auth);
-	console.log(hash);
-	hash = hash.substring(0, 9);
-	console.log(hash);
-	var username = "";
-	for(var i = 0; i < 9; i = i + 3)
-	{
-		//console.log(hash[i]);
-		var char1 = hash[i];
-		var char2 = hash[i+1];
-		var char3 = hash[i+2];
-		console.log(char1 + "|" + char2 + "|" + char3);
-
-		char1 = parseInt(Number("0x" + char1), 10);
-		char2 = parseInt(Number("0x" + char2), 10);
-		char3 = parseInt(Number("0x" + char3), 10);
-		console.log(char1 + "|" + char2 + "|" + char3);
-
-		var pos = char1*16*16 + char2*16 + char3;
-		console.log(pos);
-		pos += i*16*16*16;
-		console.log(pos);
-
-
-		var toAdd = names[pos];
-		console.log(toAdd);
-		
-		username += toAdd + " ";
-		console.log(username.substring(0, -1));
-	}
-	return username.substring(0, -1);
-}
-
 /*var stripRealAuth = function (messages)
 {
 	for(var i = 0; i < messages.length; i++)
@@ -81,11 +38,7 @@ module.exports =
 {
 	isValidAuth: isValidAuth,
 
-	isValidChatroom: isValidChatroom,
-
-	sha256: sha256,
-
-	authStringToUsername: authStringToUsername//,
+	isValidChatroom: isValidChatroom//,
 
 	//stripRealAuth: stripRealAuth
 }
--- chat.js
+++ chat.js
@@ -1,4 +1,5 @@
 var glob = require("glob");
+var crypto = require("crypto");
 
 var AUTH = require('./auth');
 var FILE = require('./file');
@@ -24,6 +25,47 @@ var currentDate = function ()
     	return d;
 }
 
+var sha256 = function (text)
+{
+	return crypto.createHash('sha256').update(text).digest('hex');
+}
+
+var authStringToUsername = function (auth, names)
+{
+	console.log(auth);
+	var hash = sha256(auth);
+	console.log(hash);
+	hash = hash.substring(0, 9);
+	console.log(hash);
+	var username = "";
+	for(var i = 0; i < 9; i = i + 3)
+	{
+		//console.log(hash[i]);
+		var char1 = hash[i];
+		var char2 = hash[i+1];
+		var char3 = hash[i+2];
+		console.log(char1 + "|" + char2 + "|" + char3);
+
+		char1 = parseInt(Number("0x" + char1), 10);
+		char2 = parseInt(Number("0x" + char2), 10);
+		char3 = parseInt(Number("0x" + char3), 10);
+		console.log(char1 + "|" + char2 + "|" + char3);
+
+		var pos = char1*16*16 + char2*16 + char3;
+		console.log(pos);
+		pos += i*16*16*16;
+		console.log(pos);
+
+
+		var toAdd = names[pos];
+		console.log(toAdd);
+		
+		username += toAdd + " ";
+		console.log(username.substring(0, -1));
+	}
+	return username.substring(0, -1);
+}
+
 var getChatroomMessages = function (chatroom)
 {
 	var chatroomFilename = chatroomNameToChatroomFilename(chatroom);
@@ -41,7 +83,7 @@ var addMessageToChatroom = function (auth, chatroom, message)
 	//console.log(chatroomData);
 
 	var names = FILE.getJsonFromTextfile("names.json");
-	auth = AUTH.authStringToUsername(auth);
+	auth = authStringToUsername(auth, names);
 
 	chatroomData.messages.push({"time":currentDate(), "user":auth, "message":message});
 	//console.log(chatroomData);
@@ -74,6 +116,10 @@ module.exports =
 	chatroomFilenameToChatroomName: chatroomFilenameToChatroomName,
 
 	currentDate: currentDate,
+
+	sha256: sha256,
+
+	authStringToUsername: authStringToUsername,
 
 	getChatroomMessages: getChatroomMessages,
 
