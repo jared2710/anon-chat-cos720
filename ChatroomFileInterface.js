2669738b344640291ede60cee4b8b846bd615296
jared2710 <u17051429@tuks.co.za>
2020-05-20 21:25:10 +0200

    Still not working come on

--- ChatroomFileInterface.js
+++ ChatroomFileInterface.js
@@ -30,8 +30,10 @@ class ChatroomFileInterface
 	addMessage(pseudonym, message)
 	{
 		var json = this.chatroomfile.getMessages();
+		console.log(json);
 
 		json.messages.push({"time":this.currentDate(), "user":pseudonym, "message":message});
+		console.log(json);
 		
 		this.chatroomfile.writeMessages(json);
 	}
